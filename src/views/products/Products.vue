1c11811c34664890b7a714cdfedf7197358158a2
Maxim Palchikov <palchmx@gmail.com>
2021-06-19 00:04:22 +0300

    Add products table

--- src/views/products/Products.vue
+++ src/views/products/Products.vue
@@ -4,7 +4,41 @@
       <v-col>
         <h1>Товары</h1>
         <v-btn :to="{ name: 'newProduct' }">Добавить</v-btn>
+        <v-data-table
+          :headers="headers"
+          :items="products"
+          :items-per-page="5"
+          class="elevation-1"
+        ></v-data-table>
       </v-col>
     </v-row>
   </v-layout>
 </template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      headers: [
+        {
+          text: "Наименование",
+          sortable: false,
+          value: "title"
+        },
+        {
+          text: "Цена",
+          sortable: false,
+          value: "price"
+        }
+      ],
+      products: []
+    };
+  },
+  async created() {
+    const response = await axios.get("http://localhost:5000/api/products");
+    this.products = response.data;
+  }
+};
+</script>
